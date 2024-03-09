6038c1a701407374a146c16df007849c97276d01
Santiago Benitez <santiago9607b@gmail.com>
2024-03-09 11:36:12 -0500

    Update for WSS

--- src/djangochatflow/settings.py
+++ src/djangochatflow/settings.py
@@ -140,6 +140,9 @@ JAZZMIN_SETTINGS = {
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels.layers.InMemoryChannelLayer",  # Use appropriate backend
+        'CONFIG': {
+            'hosts': [('wss://esesocota.benitech.cloud/ws/',)],
+        },
     },
 }
 
@@ -207,7 +210,7 @@ JAZZMIN_SETTINGS = {
     # Custom links to append to app groups, keyed on app name
     "topmenu_links": [
         # Url that gets reversed (Permissions can be added)
-        {"name": "Home",  "url": "turnos-index"},
+        {"name": "Pantalla de Turnos",  "url": "turnos-index"},
     ],
 
     # Custom icons for side menu apps/models See https://fontawesome.com/icons?d=gallery&m=free&v=5.0.0,5.0.1,5.0.10,5.0.11,5.0.12,5.0.13,5.0.2,5.0.3,5.0.4,5.0.5,5.0.6,5.0.7,5.0.8,5.0.9,5.1.0,5.1.1,5.2.0,5.3.0,5.3.1,5.4.0,5.4.1,5.4.2,5.13.0,5.12.0,5.11.2,5.11.1,5.10.0,5.9.0,5.8.2,5.8.1,5.7.2,5.7.1,5.7.0,5.6.3,5.5.0,5.4.2
